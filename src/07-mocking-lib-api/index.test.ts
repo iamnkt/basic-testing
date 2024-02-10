1263152f397a6839056eb530c924236ac40a04b3
iamnkt <sg23mj@gmail.com>
2024-02-10 16:56:05 +0300

    feat: add snapshot tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,25 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+// import { throttledGetDataFromApi } from './index';
+// import { throttle } from 'lodash';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+mockedAxios.get.mockImplementation(() => Promise.resolve({}));
+
+mockedAxios.create = jest.fn(() => mockedAxios);
 
 describe('throttledGetDataFromApi', () => {
+  jest.mock('lodash/throttle', () => ({
+    default: jest.fn((fn) => fn),
+    __esModule: true,
+  }));
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    // const spyOnCreate = jest.spyOn(mockedAxios, 'create');
+    // jest.spyOn(mockedAxios, 'get').mockImplementation(() => Promise.resolve({}));
+    // await throttledGetDataFromApi('/path');
+    // expect(spyOnCreate).toBeCalled();
   });
 
   test('should perform request to correct provided url', async () => {
